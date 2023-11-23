71f1bc604c3f7cdee106cf71443657d600518baf
Alehandro_26rus <skash96@yandex.ru>
2023-11-23 16:31:58 +0300

    Используем цикл for, чтобы найти количество строк в source, длина которых меньше или равна 3 символам

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Ввод исходного массива
+        string[] source = { "Hello", "2", "world", ":-)" };
+
+        // Находим количество строк, длина которых меньше или равна 3 символам
+        int count = 0;
+        for (int i = 0; i < source.Length; i++)
+        {
+            if (source[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+    }
+}
